828afd6318de0cd673aa2d06a5d9d648865ffe2a
pheobeayo <>
2024-09-24 18:42:31 +0100

    update Readme and Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,9 @@ const Hero = () => {
                     <BackgroundCarousel /> 
                     <figcaption className="absolute bottom-96 mx-24  gap-8">
                         <div className="lg:w-[70%] md:w-[70%] w-[100%] mx-auto text-center my-12 px-4 lg:px-0 md:px-0">
-                        <h1 className="text-white lg:text-[50px] md:text-[50px] text-[28px] font-[700] my-4 font-serif ">Empowering Vulnerable Individual and Institution</h1>
+                        <h1 className="text-white lg:text-[50px] md:text-[50px] text-[28px] font-[700] my-4 font-serif ">Empowering Vulnerable Individuals and Institutions</h1>
                         <p className="lg:text-[24px] md:text-[24px] text-[18px] text-white">Transparent and impactful crowdfunding for
-                            <br />  disadvantaged individuals and organisation
+                            <br />  disadvantaged individuals and organisations
                             <br /> across Africa
                         </p>
                         <div className="mt-4">
